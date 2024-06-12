7d2c264951e5f477f61f9733ce7c7a0a54c04cd0
n-nizar <167634786+n-nizar@users.noreply.github.com>
2024-06-12 11:52:05 -0400

    Updated transform_json_data.sql

--- Data_Transformation/transform_json_data.sql
+++ Data_Transformation/transform_json_data.sql
@@ -1,54 +1,40 @@
-USE ROLE accountadmin;
-
-CREATE WAREHOUSE IF NOT EXISTS transform_wh WITH warehouse_size='x-small';
-CREATE DATABASE IF NOT EXISTS data_transformation;
-
--- Replace "nnizar" with your specific Snowflake username.
--- Optional: the "nnsuperadmin" role is used to manage all of my key data projects.
-CREATE ROLE IF NOT EXISTS nnsuperadmin;
-GRANT ROLE nnsuperadmin TO USER nnizar;
-CREATE ROLE IF NOT EXISTS dengineer;
-GRANT ROLE dengineer TO ROLE nnsuperadmin;
-GRANT ROLE dengineer TO USER nnizar;
-
-GRANT ROLE dengineer TO USER nnizar;
-GRANT ROLE dengineer TO ROLE nnsuperadmin;
-
-GRANT USAGE ON WAREHOUSE transform_wh TO ROLE dengineer;
-GRANT USAGE, CREATE SCHEMA ON DATABASE data_transformation TO ROLE dengineer;
-
+-- Refer to Security_and_Access_Control/setup_transform_json_data.sql for the initial setup.
+-- Use the dengineer role
 USE ROLE dengineer;
 
+-- Create schema for semi-structured data
 CREATE SCHEMA IF NOT EXISTS data_transformation.semistructured;
 
--- Create an internal stage
+-- Create an internal stage with directory enabled
 CREATE STAGE IF NOT EXISTS data_transformation.semistructured.intstage_bulkload
-directory = (enable = TRUE);
+  DIRECTORY = (ENABLE = TRUE);
 
--- Create "demo_sales_json" table to bulk load data from a file in a stage
+-- Create a table to bulk load JSON data
 CREATE TABLE IF NOT EXISTS data_transformation.semistructured.demo_sales_json (
-    data VARIANT);
+    data VARIANT
+);
 
--- Load local files to an internal stage using SnowSQL. Modify the file path according to where you have saved the files.
+-- Load local files to internal stage using SnowSQL
+-- Uncomment and modify the file path according to your local setup
 /*
 PUT file:///Users/helloworld/Documents/Workspace/Development/GitHub/nn-snowflake/snowflake-scripts/Data_Transformation/sample_data/sample_json_data_sales.json
 @data_transformation.semistructured.intstage_bulkload/json/sample_data;
 */
 
--- Load files from an internal stage to a Snowflake table.
+-- Load files from an internal stage to a Snowflake table
 COPY INTO data_transformation.semistructured.demo_sales_json
 FROM @data_transformation.semistructured.intstage_bulkload/json/sample_data/sample_json_data_sales.json
 FILE_FORMAT = (TYPE = 'JSON')
 ON_ERROR = 'CONTINUE';
 
--- After loading data from stage to Snowflake table, remove it from the Stage
+-- Remove files from the stage after loading
 REMOVE @data_transformation.semistructured.intstage_bulkload/json/sample_data/sample_json_data_sales.json;
 
--- Confirm that the file has been removed
+-- Confirm that the files have been removed
 LIST @data_transformation.semistructured.intstage_bulkload/json/sample_data/;
 
-CREATE VIEW IF NOT EXISTS data_transformation.semistructured.vw_demo_sales_customers
-AS
+-- Create views for transformed sales data
+CREATE VIEW IF NOT EXISTS data_transformation.semistructured.vw_demo_sales_customers AS
 SELECT  c.value:customer_id::INTEGER                    AS cust_id,
         c.value:name::STRING                            AS name,
         c.value:email::STRING                           AS email,
@@ -60,11 +46,10 @@ SELECT  c.value:customer_id::INTEGER                    AS cust_id,
         c.value:created_at::TIMESTAMPTZ                 AS created_at
 FROM data_transformation.semistructured.demo_sales_json AS s,
 LATERAL FLATTEN (input => s.data:customers) AS c
-WHERE EXTRACT("Year", c.value:created_at::TIMESTAMPTZ) = '2023'
-AND EXTRACT("Month", c.value:created_at::TIMESTAMPTZ) = '01';
+WHERE EXTRACT(YEAR, c.value:created_at::TIMESTAMPTZ) = 2023
+  AND EXTRACT(MONTH, c.value:created_at::TIMESTAMPTZ) = 1;
 
-CREATE VIEW IF NOT EXISTS data_transformation.semistructured.vw_demo_sales_orders
-AS
+CREATE VIEW IF NOT EXISTS data_transformation.semistructured.vw_demo_sales_orders AS
 SELECT  o.value:order_id::INTEGER                       AS order_id,
         o.value:order_date::TIMESTAMPTZ                 AS order_date,
         o.value:customer_id::INTEGER                    AS customer_id,
@@ -77,6 +62,7 @@ FROM data_transformation.semistructured.demo_sales_json AS s,
 LATERAL FLATTEN (input => s.data:orders) AS o,
 LATERAL FLATTEN (input => o.value:products) AS op;
 
+CREATE VIEW IF NOT EXISTS data_transformation.semistructured.vw_demo_sales_products AS
 SELECT  p.value:product_id::INTEGER                     AS product_id,
         p.value:name::STRING                            AS name,
         p.value:description::STRING                     AS description,
@@ -86,30 +72,32 @@ SELECT  p.value:product_id::INTEGER                     AS product_id,
 FROM data_transformation.semistructured.demo_sales_json AS s,
 LATERAL FLATTEN (input => s.data:products) AS p;
 
--- Create "demo_library_json" table to bulk load data from a file in a stage
+-- Create a table to bulk load JSON data for library
 CREATE TABLE IF NOT EXISTS data_transformation.semistructured.demo_library_json (
-    data VARIANT);
+    data VARIANT
+);
 
--- Load local files to an internal stage using SnowSQL. Modify the file path according to where you have saved the files.
+-- Load local files to internal stage using SnowSQL
+-- Uncomment and modify the file path according to your local setup
 /*
 PUT file:///Users/helloworld/Documents/Workspace/Development/GitHub/nn-snowflake/snowflake-scripts/Data_Transformation/sample_data/sample_json_data_library.json 
 @data_transformation.semistructured.intstage_bulkload/json/sample_data;
 */
 
--- Load files from an internal stage to a Snowflake table using SnowSQL
+-- Load files from an internal stage to a Snowflake table
 COPY INTO data_transformation.semistructured.demo_library_json
 FROM @data_transformation.semistructured.intstage_bulkload/json/sample_data/sample_json_data_library.json
 FILE_FORMAT = (TYPE = 'JSON')
 ON_ERROR = 'CONTINUE';
 
--- After loading data from stage to Snowflake table, remove it from the Stage
+-- Remove files from the stage after loading
 REMOVE @data_transformation.semistructured.intstage_bulkload/json/sample_data/sample_json_data_library.json;
 
--- Confirm that the file has been removed
+-- Confirm that the files have been removed
 LIST @data_transformation.semistructured.intstage_bulkload/json/sample_data/;
 
-CREATE VIEW IF NOT EXISTS data_transformation.semistructured.vw_demo_library_books
-AS
+-- Create views for transformed library data
+CREATE VIEW IF NOT EXISTS data_transformation.semistructured.vw_demo_library_books AS
 SELECT  b.value:book_id::INTEGER                        AS book_id,
         b.value:title::STRING                           AS title,
         b.value:author_id::INTEGER                      AS author_id,
@@ -117,33 +105,31 @@ SELECT  b.value:book_id::INTEGER                        AS book_id,
         b.value:availability::STRING                    AS availability,
         b.value:isbn::INTEGER                           AS isbn,
         b.value:published_date::DATE                    AS published_date
-FROM data_transformation.semistructured.demo_library_json lib,
-LATERAL FLATTEN (input => lib.data:library_data) as ld,
-LATERAL FLATTEN (input => ld.value:data) as b
-WHERE (ld.value:object::STRING) = 'books';
+FROM data_transformation.semistructured.demo_library_json AS lib,
+LATERAL FLATTEN (input => lib.data:library_data) AS ld,
+LATERAL FLATTEN (input => ld.value:data) AS b
+WHERE ld.value:object::STRING = 'books';
 
-CREATE VIEW IF NOT EXISTS data_transformation.semistructured.vw_demo_library_authors
-AS
+CREATE VIEW IF NOT EXISTS data_transformation.semistructured.vw_demo_library_authors AS
 SELECT  a.value:author_id::INTEGER                      AS author_id,
         a.value:name::STRING                            AS name,
         a.value:nationality::STRING                     AS nationality,
         a.value:birth_date::DATE                        AS birth_date,
         a.value:death_date::DATE                        AS death_date
-FROM data_transformation.semistructured.demo_library_json lib,
-LATERAL FLATTEN (input => lib.data:library_data) as ld,
-LATERAL FLATTEN (input => ld.value:data) as a
+FROM data_transformation.semistructured.demo_library_json AS lib,
+LATERAL FLATTEN (input => lib.data:library_data) AS ld,
+LATERAL FLATTEN (input => ld.value:data) AS a
 WHERE ld.value:object::STRING = 'authors'
-AND a.value:nationality::STRING = 'American';
+  AND a.value:nationality::STRING = 'American';
 
-CREATE VIEW IF NOT EXISTS data_transformation.semistructured.vw_demo_library_members
-AS
+CREATE VIEW IF NOT EXISTS data_transformation.semistructured.vw_demo_library_members AS
 SELECT  m.value:member_id::INTEGER                      AS member_id,
         m.value:name::STRING                            AS name,
         m.value:email::STRING                           AS email,
         m.value:phone::STRING                           AS phone,
         m.value:address::STRING                         AS address,
-        m.value:membership_start_date::DATE AS membership_start_date
-FROM data_transformation.semistructured.demo_library_json lib,
-LATERAL FLATTEN (lib.data:library_data) AS ld,
-LATERAL FLATTEN (ld.value:data) AS m
+        m.value:membership_start_date::DATE             AS membership_start_date
+FROM data_transformation.semistructured.demo_library_json AS lib,
+LATERAL FLATTEN (input => lib.data:library_data) AS ld,
+LATERAL FLATTEN (input => ld.value:data) AS m
 WHERE ld.value:object = 'members';
